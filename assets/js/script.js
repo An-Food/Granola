f2e9a309ec48c7c2471333a50255162f28019cc7
AnFood <anfoodgranola@gmail.com>
2022-10-06 22:47:02 +0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,10 +156,94 @@ $(document).ready(function(){
 
   // modal code
   $('.btn-login_signup').on('click', function(){
+    $('.auth-form__input').val('');
     $('.modal-container').css("display", "block");
   })
 
   $('.btn-close').on('click', function(){
     $('.modal-container').css("display", "none");
+    $('.password_forgot').css("display", "none");
+    $('.auth-form__help--forgot').css("display", "block");
   })
+
+  $('.modal__overlay').click(function(){
+    $('.modal-container').css("display", "none");
+    $('.password_forgot').css("display", "none");
+    $('.auth-form__help--forgot').css("display", "block");
+  }) 
+
+  // quên password
+  $('.auth-form__help--forgot').click(function(){
+    $('.password_forgot').css("display", "block");
+    $('.auth-form__help--forgot').css("display", "none");
+  })
+
+  // Tạo tài khoản
+  var m = 0;
+  const user_name = [];
+  const password = [];
+  const email = [];
+  $('.btn-signup').click(function(){
+    var F = 0;       
+        if(!$('.auth-form__input-username').val()){
+            $('.auth-form__input-username').addClass("empty_field");
+            $('.auth-form__input-username').attr("placeholder", "Không được để trống!");        
+            F = 1;    
+        }    
+
+        if(!$('.auth-form__input-email').val()){
+            $('.auth-form__input-email').addClass("empty_field");
+            $('.auth-form__input-email').attr("placeholder", "Không được để trống!");
+            F = 1;
+            
+        }
+
+        if(!$('.auth-form__input-password').val()){
+            $('.auth-form__input-password').addClass("empty_field");
+            $('.auth-form__input-password').attr("placeholder", "Không được để trống!");        
+            F = 1;    
+        }
+
+        if(!$('.auth-form__input-repassword').val()){
+            $('.auth-form__input-repassword').addClass("empty_field");
+            $('.auth-form__input-repassword').attr("placeholder", "Không được để trống!");     
+            F = 1;       
+        }
+        
+        if(m >0){
+            for(let i=0; i<m; i++){
+                if($('.auth-form__input-email').val() == user_name[i]){
+                    $('.auth-form__input-email').addClass("empty_field");
+                    $('.auth-form__input-email').val('');
+                    $('.auth-form__input-email').attr("placeholder", "Tên tài khoản này đã được sử dụng!");
+                    F = 1;
+                }
+                if($('.auth-form__input-email').val() == email[i]){
+                  $('.auth-form__input-email').addClass("empty_field");
+                  $('.auth-form__input-email').val('');
+                  $('.auth-form__input-email').attr("placeholder", "Email này đã được sử dụng!");
+                  F = 1;
+                }
+            }
+        }
+
+        if($('.auth-form__input-repassword').val() != $('.auth-form__input-password').val()){
+            $('.auth-form__input-repassword').addClass("empty_field");
+            $('.auth-form__input-repassword').val('');
+            $('.auth-form__input-repassword').attr("placeholder", "Mật khẩu nhập lại không giống!");  
+            F = 1;   
+        }
+
+        if(F == 0){
+            user_name[m] = $('.auth-form__input-username').val();
+            password[m] = $('.auth-form__input-password').val(); 
+            email[m] = $('.auth-form__input-emmail').val(); 
+            m++;
+            alert("Đăng ký thành công!");  
+            $('.modal-container').css("display", "none");
+            $('.password_forgot').css("display", "none");
+            $('.auth-form__help--forgot').css("display", "block");
+        }
+  })
+
 });
